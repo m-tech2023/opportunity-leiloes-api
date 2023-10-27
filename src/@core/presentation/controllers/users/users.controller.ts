168fabf8a1ba69d52291b10cd7186dfe12e8364e
Cristiano <cristiano.justino@m-tech.digital>
2023-10-27 19:06:28 -0300

    fix: users.controller.ts

--- src/@core/presentation/controllers/users/users.controller.ts
+++ src/@core/presentation/controllers/users/users.controller.ts
@@ -19,9 +19,7 @@ import { DestroyUserUseCase } from 'src/@core/application/use-cases/users/destro
 import { FindByIdUseCase } from 'src/@core/application/use-cases/users/find-by-id.usecase';
 import { GetAllUseCase } from 'src/@core/application/use-cases/users/get-all.usecase';
 import { UpdateUserUseCase } from 'src/@core/application/use-cases/users/update-user.usecase';
-import { AccessLogService } from '../../../application/services/access-log/access-log.service';
 // import { AuthorizationGuard } from 'src/@core/infra/frameworks/nestjs/modules/auth/guards/authorization/authorization.guard';
-import { AuthorizationGuard } from 'src/@core/infra/frameworks/nestjs/modules/auth/guards/authorization/authorization.guard';
 import { AccessLogService } from '../../../application/services/access-log/access-log.service';
 // import { AuthorizationGuard } from 'src/@core/infra/frameworks/nestjs/modules/auth/guards/authorization/authorization.guard';
 import { AuthorizationGuard } from 'src/@core/infra/frameworks/nestjs/modules/auth/guards/authorization/authorization.guard';
